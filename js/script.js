3a33984fab31c7af60e521eaa8a56b600766e35b
Lucas <lucas.faustmann@gmail.com>
2023-01-30 08:14:36 +0100

    Commit initial Ajout de l'architecture du site (CSS, JS, HTML)

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function afficherStatsGrandsPrix () 
+{
+    const divStatsGrandsPrix = document.querySelector("#stats");
+    const sectionStatsGrandsPrix = document.createElement("section");
+    sectionStatsGrandsPrix.innerHTML += "<h2>Analyse des données relatives aux Grands prix</h2>";
+    const figureStatsGrandsPrix = document.createElement("figure");
+    figureStatsGrandsPrix.innerHTML = "<img src='graphe.png'>";
+
+    divStatsGrandsPrix.appendChild(sectionStatsGrandsPrix);
+    sectionStatsGrandsPrix.appendChild(figureStatsGrandsPrix);
+
+}
+
+
+const boutonDecouvrirStatsGrandsPrix = document.querySelector(".btn-decouvrir-stats-grands-prix");
+boutonDecouvrirStatsGrandsPrix.addEventListener("click", function() {
+    document.querySelector("#stats").innerHTML = "";
+    afficherStatsGrandsPrix();
+});
