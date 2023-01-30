569ac69b19a891e6d68d44e5d487115b864d58b3
Lucas <lucas.faustmann@gmail.com>
2023-01-30 10:21:09 +0100

    npm init et installation de chart.js et prettier

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-function afficherStatsGrandsPrix () 
-{
-    const divStatsGrandsPrix = document.querySelector("#stats");
-    const sectionStatsGrandsPrix = document.createElement("section");
-    sectionStatsGrandsPrix.innerHTML += "<h2>Analyse des données relatives aux Grands prix</h2>";
-    const figureStatsGrandsPrix = document.createElement("figure");
-    figureStatsGrandsPrix.innerHTML = "<img src='data/graphe.png'>";
+// import {listes des fonctions à importer} from "./fichier.js";
 
-    divStatsGrandsPrix.appendChild(sectionStatsGrandsPrix);
-    sectionStatsGrandsPrix.appendChild(figureStatsGrandsPrix);
+function afficherStatsGrandsPrix() {
+  const divStatsGrandsPrix = document.querySelector("#stats");
+  const sectionStatsGrandsPrix = document.createElement("section");
+  sectionStatsGrandsPrix.innerHTML +=
+    "<h2>Analyse des données relatives aux Grands prix</h2>";
+  const figureStatsGrandsPrix = document.createElement("figure");
+  figureStatsGrandsPrix.innerHTML = "<img src='data/graphe.png'>";
 
+  divStatsGrandsPrix.appendChild(sectionStatsGrandsPrix);
+  sectionStatsGrandsPrix.appendChild(figureStatsGrandsPrix);
 }
 
-
-const boutonDecouvrirStatsGrandsPrix = document.querySelector(".btn-decouvrir-stats-grands-prix");
-boutonDecouvrirStatsGrandsPrix.addEventListener("click", function() {
-    document.querySelector("#stats").innerHTML = "";
-    afficherStatsGrandsPrix();
+const boutonDecouvrirStatsGrandsPrix = document.querySelector(
+  ".btn-decouvrir-stats-grands-prix"
+);
+boutonDecouvrirStatsGrandsPrix.addEventListener("click", function () {
+  document.querySelector("#stats").innerHTML = "";
+  afficherStatsGrandsPrix();
 });
