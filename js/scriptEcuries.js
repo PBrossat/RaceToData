86afee6e94ab9d1f913b22b343da85c9cbcf4efb
Matis Brun <matis.brun@etu.univ-lyon1.fr>
2023-01-30 21:28:30 +0100

    tentative de fetch

--- js/scriptEcuries.js
+++ js/scriptEcuries.js
@@ -1,20 +1,23 @@
-function afficherStatsEcuries () 
-{
-    const divStatsEcuries = document.querySelector("#stats");
-    const sectionStatsEcuries = document.createElement("section");
-    sectionStatsEcuries.innerHTML += "<h2>Analyse des données relatives aux Ecuries</h2>";
-    const figureStatsEcuries = document.createElement("figure");
-    figureStatsEcuries.innerHTML = "<img src='data/graphe.png'>";
+function afficherStatsEcuries() {
+  const divStatsEcuries = document.querySelector("#stats");
+  const sectionStatsEcuries = document.createElement("section");
+  sectionStatsEcuries.innerHTML +=
+    "<h2>Analyse des données relatives aux Ecuries</h2>";
+  const figureStatsEcuries = document.createElement("figure");
+  figureStatsEcuries.innerHTML = "";
 
-    divStatsEcuries.appendChild(sectionStatsEcuries);
-    sectionStatsEcuries.appendChild(figureStatsEcuries);
-
+  divStatsEcuries.appendChild(sectionStatsEcuries);
+  sectionStatsEcuries.appendChild(figureStatsEcuries);
 }
 
-
-const boutonDecouvrirStatsEcuries = document.querySelector(".btn-decouvrir-stats-ecuries");
-boutonDecouvrirStatsEcuries.addEventListener("click", function() {
-    document.querySelector("#stats").innerHTML = "";
-    afficherStatsEcuries();
+const boutonDecouvrirStatsEcuries = document.querySelector(
+  ".btn-decouvrir-stats-ecuries"
+);
+boutonDecouvrirStatsEcuries.addEventListener("click", function () {
+  document.querySelector("#stats").innerHTML = "";
+  afficherStatsEcuries();
 });
 
+fetch("http://ergast.com/api/f1/constructors/mclaren/")
+  .then((response) => response.json())
+  .then((data) => console.nationality(data));
