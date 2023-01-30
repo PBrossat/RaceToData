808826fb5572f19485ae70a69e0df40738651219
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-01-30 23:28:41 +0100

    Enrichissement de scriptDriver.js
    Création de nombreuses fonction permettant de rendre plus lisible le
    code + Création d'une fonction permettant de creer un autre
    graphe en baton + formulaire permettatn à l'utilisateur de choisir
    le graphe qu'il voudra afficher (non fonctionnel).

--- js/scriptDriver.js
+++ js/scriptDriver.js
@@ -6,6 +6,13 @@ function afficherStatsPilotes()
     sectionStatsPilotes.innerHTML += "<h2>Analyse des données relatives aux Pilotes</h2>";
     const divStatsPilotes = document.createElement("div");
     divStatsPilotes.id="divId"
+    divStatsPilotes.innerHTML=`
+    <select class="choixGraphique">
+        <option value="">Type de graphique</option>
+        <option value="radar">Radar</option>
+        <option value="baton">Bâton</option>
+    </select>
+  `;
     const graphique=document.createElement("canvas");
     graphique.id="CanvaId";
 
@@ -22,117 +29,205 @@ const boutonDecouvrirStatsPilotes = document.querySelector(".btn-decouvrir-stats
 boutonDecouvrirStatsPilotes.addEventListener("click", function() {
     document.querySelector("#stats").innerHTML = "";
     afficherStatsPilotes();
-    grapheDriverPointMoyenParGP();
 
+    // const selectGraphique = document.querySelector(".choixGraphique");
+    // selectGraphique.addEventListener("click", function() {
+    //     if (selectGraphique=="radar")
+    //     {
+    //         grapheDriverPointMoyenParGPRadar();
+    //     }
+    //     else if (selectGraphique=="baton")
+    //     {
+    //         grapheDriverPointMoyenParGPBaton();
+    //     }
+    //     else 
+    //     {
+    //         alert("Veuillez selectionner un type de graphique")
+    //     }
+
+    // })
+    grapheDriverPointMoyenParGPRadar();
 });
 
 
-function grapheDriverPointMoyenParGP()
-{
-    //Récuperer les données dans un tableau
-    fetch('json/Driver.json')
+//Récuperer les données dans un tableau globalTabData accessible partout dans le script 
+let globalTabData;
+fetch('json/Driver.json')
   .then(response => response.json())
   .then(data => {
     const piloteData = data;
+    const tabData = piloteData.map(pilote => {
+      return {
+        Nom: pilote["Name"],
+        Age: pilote["âge"],
+        nbMoyenPointsParGP: pilote["Nb points moyen/GP"],
+        nbVictoires: pilote["Nb Victoires"],
+        nbPodiums: pilote["Nb podiums"],
+        nbDNF: pilote["Nb DNF"],
+        nbDnfMoyenParSaison: pilote["Pourcent DNF moyen/GP"],
+        nbGpDisputes: pilote["NB GP disputés"]
+      };
+    });
+    globalTabData = tabData;
+    return tabData;
+  })
 
-    const tabData = piloteData.map(pilote =>{
-        return {
-                Nom: pilote["Name"],
-                nbMoyenPointsParGP: pilote["Nb points moyen/GP"]
-            };
-        }
-    );
-    
 
-    //création d'un tableau NomPilote  
-    const NomPilote=[]
-    for (var i=0; i<tabData.length; i++)
+// Création de fonctions permettants de récuperer (à partir de globalTabData) des informations et de les stockers dans des tableaux
+function getNomPilote()
+{
+    const nomPilote=[];
+    for (var i=0; i<globalTabData.length; i++)
     {
-        NomPilote.push(tabData[i].Nom);
+        nomPilote.push(globalTabData[i].Nom);
     }
+    return nomPilote;
+}
 
-    //création d'un tableau nbMoyenPointsParGpParPilote  
-    const nbMoyenPointsParGpParPilote=[]
-    for (var i=0; i<tabData.length; i++)
+
+function getAgePilote()
+{
+    const agePilote=[];
+    for (var i=0; i<globalTabData.length; i++)
     {
-        nbMoyenPointsParGpParPilote.push(tabData[i].nbMoyenPointsParGP);
+        agePilote.push(globalTabData[i].Age);
     }
+    return agePilote;
+}
 
+
+function getNbMoyenPointsParGP()
+{
+    const nbMoyenPointsParGP=[];
+    for (var i=0; i<globalTabData.length; i++)
+    {
+        nbMoyenPointsParGP.push(globalTabData[i].nbMoyenPointsParGP);
+    }
+    return nbMoyenPointsParGP;
+}
+
+function getNbVictoires()
+{
+    const nbVictoires=[];
+    for (var i=0; i<globalTabData.length; i++)
+    {
+        nbVictoires.push(globalTabData[i].nbVictoires);
+    }
+    return nbVictoires;
+}
+
+function getNbPodiums()
+{
+    const nbPodiums=[];
+    for (var i=0; i<globalTabData.length; i++)
+    {
+        nbPodiums.push(globalTabData[i].nbPodiums);
+    }
+    return nbPodiums;
+}
+
+function getNbDNF()
+{
+    const nbDNF=[];
+    for (var i=0; i<globalTabData.length; i++)
+    {
+        nbDNF.push(globalTabData[i].nbDNF);
+    }
+    return nbDNF;
+}
+
+function getNbDnfMoyenParSaison()
+{
+    const nbDnfMoyenParSaison=[];
+    for (var i=0; i<globalTabData.length; i++)
+    {
+        nbDnfMoyenParSaison.push(globalTabData[i].nbDnfMoyenParSaison);
+    }
+    return nbDnfMoyenParSaison;
+}
+
+function getNbGpDisputes()
+{
+    const nbGpDisputes=[];
+    for (var i=0; i<globalTabData.length; i++)
+    {
+        nbGpDisputes.push(globalTabData[i].nbGpDisputes);
+    }
+    return nbGpDisputes;
+}
+
+
+// fonction permettant d'afficher un graphe 
+function grapheDriverPointMoyenParGPRadar()
+{
+
+    const nomPilote=getNomPilote();
+    const nbMoyenPointsParGpParPilote=getNbMoyenPointsParGP();
 
     const configuration = 
     {
         type: 'radar',
         data: 
         {
-            labels: NomPilote,
+            labels: nomPilote,
 
             datasets: [{
                 label: "Points moyen par Grand Prix ",
                 data: nbMoyenPointsParGpParPilote,
+                fill: true,
                 backgroundColor: 'rgba(255, 99, 132, 0.2)', 
-                borderColor: 'rgb(255, 99, 132)',
-                pointBackgroundColor: 'rgb(255, 99, 132)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
+                borderColor: 'rgb(0, 0, 0)',
+                pointBackgroundColor: 'rgb(255,0,0)',
                 pointHoverBorderColor: 'rgb(255, 99, 132)'
 
-            }]
-        },   
+            }],
+        }, 
+          
     };
 
+
     const graphique = document.getElementById('CanvaId');
-    const chart = new Chart(graphique, configuration)
+    const chart = new Chart(graphique, configuration) 
+}
 
 
-      
-});
+
+function grapheDriverPointMoyenParGPBaton()
+{
+    //création d'un tableau de couleurs aléatoires
+    var tabCouleurs = [];
+    for (var i = 0; i < globalTabData.length; i++) {
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        tabCouleurs.push(`rgba(${r}, ${g}, ${b}, 0.9)`);
+    }
+
+    const nomPilote=getNomPilote();
+    const nbMoyenPointsParGpParPilote=getNbMoyenPointsParGP();
+
+    const configuration = 
+    {
+        type: 'bar',
+        data: 
+        {
+            labels: nomPilote,
+
+            datasets: [{
+                label: "Points moyen par Grand Prix ",
+                data: nbMoyenPointsParGpParPilote,
+                backgroundColor:tabCouleurs,
+                borderColor: 'rgb(0, 0, 0)'
+            }],
+        }, 
+          
+    };
+    const graphique = document.getElementById('CanvaId');
+    const chart = new Chart(graphique, configuration) 
 }
 
 
 
 
 
-
-
-
-
-
-
-// async function getPointsPilotesSaison(nomPilote) 
-// {
-//     let pointsParSaison = [];
-//     const API_URL = `http://ergast.com/api/f1`;
-//     const premiereSaison= data.MRData.StandingsTable.StandingsLists[0].season;
-//     for (let annee = 1950; annee <= new Date().getFullYear(); annee++) 
-//     {
-//       const response = await fetch(`${API_URL}/${annee}/drivers/${nomPilote}/driverStandings.json`);
-//       console.log(response);
-//     }
-//       const data = await response.json();
-//       const points = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].points;
-//       pointsParSaison.push({ annee, points });
-//     }
-  
-//     return pointsParSaison;
-// }
-
-// function getPremierSaisonPilote(nomPilote) {
-//     const url = `https://ergast.com/api/f1/driverStandings/1.json`;
-  
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(data => {
-//         const standingsLists = data.MRData.StandingsTable.StandingsLists;
-//         for (let i = 0; i < standingsLists.length; i++) {
-//           const driverStandings = standingsLists[i].DriverStandings;
-//           for (let j = 0; j < driverStandings.length; j++) {
-//             const driver = driverStandings[j].Driver;
-//             if (`${driver.givenName} ${driver.familyName}` === nomPilote) {
-//                 console.log(`The first season year of ${driverName} is ${standingsLists[i].season}.`);
-//                 return;
-//             }
-//           }
-//         }
-//       });
-// }
 
