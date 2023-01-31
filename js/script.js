9b65bac6c35c0be14ddf12cd1fc7045525e5a8cf
Matis Brun <matis.brun@etu.univ-lyon1.fr>
2023-01-31 14:47:12 +0100

    requete json qui marche

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-function afficherStatsGrandsPrix () 
-{
-    const divStatsGrandsPrix = document.querySelector("#stats");
-    const sectionStatsGrandsPrix = document.createElement("section");
-    sectionStatsGrandsPrix.innerHTML += "<h2>Analyse des données relatives aux Grands prix</h2>";
-    const figureStatsGrandsPrix = document.createElement("figure");
-    figureStatsGrandsPrix.innerHTML = "<img src='data/graphe.png'>";
+function afficherStatsGrandsPrix() {
+  const divStatsGrandsPrix = document.querySelector("#stats");
+  const sectionStatsGrandsPrix = document.createElement("section");
+  sectionStatsGrandsPrix.innerHTML +=
+    "<h2>Analyse des données relatives aux Grands prix</h2>";
+  const figureStatsGrandsPrix = document.createElement("figure");
+  figureStatsGrandsPrix.innerHTML = "<img src='data/graphe.png'>";
 
-    divStatsGrandsPrix.appendChild(sectionStatsGrandsPrix);
-    sectionStatsGrandsPrix.appendChild(figureStatsGrandsPrix);
-
+  divStatsGrandsPrix.appendChild(sectionStatsGrandsPrix);
+  sectionStatsGrandsPrix.appendChild(figureStatsGrandsPrix);
 }
 
-
-const boutonDecouvrirStatsGrandsPrix = document.querySelector(".btn-decouvrir-stats-grands-prix");
-boutonDecouvrirStatsGrandsPrix.addEventListener("click", function() {
-    document.querySelector("#stats").innerHTML = "";
-    afficherStatsGrandsPrix();
+const boutonDecouvrirStatsGrandsPrix = document.querySelector(
+  ".btn-decouvrir-stats-grands-prix"
+);
+boutonDecouvrirStatsGrandsPrix.addEventListener("click", function () {
+  document.querySelector("#stats").innerHTML = "";
+  afficherStatsGrandsPrix();
 });
-
--- js/scriptEcuries.js
+++ js/scriptEcuries.js
@@ -1,19 +1,24 @@
-const api = "http://ergast.com/api/f1/";
-
-// Utility functions
-const json = (res) => res.json();
-const error = (err) => console.log("Request failed: ", err);
+// const api = "http://ergast.com/api/f1/";
 
 function afficherStatsEcuries() {
-  const divStatsEcuries = document.querySelector("#stats");
+  const StatsEcuries = document.querySelector("#stats");
   const sectionStatsEcuries = document.createElement("section");
   sectionStatsEcuries.innerHTML +=
     "<h2>Analyse des données relatives aux Ecuries</h2>";
-  const figureStatsEcuries = document.createElement("figure");
-  figureStatsEcuries.innerHTML = "";
+  const divStatsEcuries = document.createElement("div");
+  divStatsEcuries.id = "divId";
+  divStatsEcuries.innerHTML = `
+    <select class="choixGraphique">
+        <option value="">Type de graphique</option>
+        <option value="radar">Radar</option>
+    </select>
+  `;
+  const graphique = document.createElement("canvas");
+  graphique.id = "CanvaId";
 
-  divStatsEcuries.appendChild(sectionStatsEcuries);
-  sectionStatsEcuries.appendChild(figureStatsEcuries);
+  StatsEcuries.appendChild(sectionStatsEcuries);
+  sectionStatsEcuries.appendChild(divStatsEcuries);
+  divStatsEcuries.appendChild(graphique);
 }
 
 const boutonDecouvrirStatsEcuries = document.querySelector(
@@ -24,11 +29,71 @@ boutonDecouvrirStatsEcuries.addEventListener("click", function () {
   afficherStatsEcuries();
 });
 
-let url = api;
-fetch(url + ".json")
+//let url = api;
+fetch("json/Ecuries.json")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+let globalTabData;
+fetch("json/Ecuries.json")
+  .then((response) => response.json())
+  .then((json) => {
+    const ecurieData = json;
+    const tabData = ecurieData.map((ecurie) => {
+      return {
+        Nom: ecurie["Name"],
+        Voiture: ecurie["car"],
+        Titres: ecurie["titles"],
+        Description: ecurie["description"],
+      };
+    });
+    globalTabData = tabData;
+    return tabData;
+  });
 
 // fetch("https://jsonplaceholder.typicode.com/todos/1")
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
+
+function getEcuries() {
+  const nomEcurie = [];
+  for (var i = 0; i < globalTabData.length; i++) {
+    nomEcurie.push(globalTabData[i].Nom);
+  }
+  return nomEcurie;
+}
+
+function getNbTitresEcuries() {
+  const nomEcurie = [];
+  for (var i = 0; i < globalTabData.length; i++) {
+    nomEcurie.push(globalTabData[i].titles);
+  }
+  return nomEcurie;
+}
+
+function grapheEcurieNbTitre() {
+  const nomEcurie = getEcuries();
+  const nbTitresEcuries = getNbTitresEcuries();
+
+  const configuration = {
+    type: "radar",
+    data: {
+      labels: nomEcurie,
+
+      datasets: [
+        {
+          label: "Nombre de titres par écurie",
+          data: nbTitresEcuries,
+          fill: true,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgb(0, 0, 0)",
+          pointBackgroundColor: "rgb(255,0,0)",
+          pointHoverBorderColor: "rgb(255, 99, 132)",
+        },
+      ],
+    },
+  };
+
+  const graphique = document.getElementById("CanvaId");
+  const chart = new Chart(graphique, configuration);
+}
