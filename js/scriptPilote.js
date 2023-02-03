e9cae73841ee9c4fb014f668fa17776e56b8583c
Lucas <lucas.faustmann@gmail.com>
2023-02-03 10:43:14 +0100

    regroupement des images pilotes dans un sous dossier pilotes dans data

--- js/scriptPilote.js
+++ js/scriptPilote.js
@@ -267,6 +267,15 @@ function mapPilote() {
     }
   );
 
+  //TODO Change map
+
+  // var layer = new L.StamenTileLayer("toner");
+  // var map = new L.Map("element_id", {
+  //     center: new L.LatLng(37.7, -122.4),
+  //     zoom: 12
+  // });
+  // map.addLayer(layer);
+
   layerPrincipale.addTo(mapPilote);
 
   //création de limite dans la map pour ne pas scroller à l'infini
