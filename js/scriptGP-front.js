205405362ae0713337b778c4cafeb39154277150
Lucas <lucas.faustmann@gmail.com>
2023-02-05 12:41:47 +0100

    ajout de la carte pour les grands-prix avec pour l'instant un seul marker fonctionnel et cliquable avec zoom

--- js/scriptGP-front.js
+++ js/scriptGP-front.js
@@ -1,4 +1,5 @@
 function afficherMapGP() {
+  //Création des div nécessaires
   const sectionStats = document.querySelector("#stats");
   const divMapEtInfosGP = document.createElement("div");
   divMapEtInfosGP.id = "map-et-infos-gp";
@@ -6,6 +7,7 @@ function afficherMapGP() {
   const divMapGP = document.createElement("div");
   divMapEtInfosGP.appendChild(divMapGP);
   divMapGP.id = "mapGP";
+  //Création de la map
   var mapGP = L.map("mapGP", {
     center: [48.866667, 2.333333],
     zoom: 1.5,
@@ -13,7 +15,7 @@ function afficherMapGP() {
   const layerPrincipale = L.tileLayer(
     "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png",
     {
-      maxZoom: 15,
+      maxZoom: 12,
       minZoom: 1.5,
 
       attribution:
@@ -21,14 +23,24 @@ function afficherMapGP() {
     }
   );
   layerPrincipale.addTo(mapGP);
+  //Création des limites de la map
   var sudOuest = L.latLng(-90, -180),
     nordEst = L.latLng(90, 180),
     limiteMap = L.latLngBounds(sudOuest, nordEst);
-
   mapGP.setMaxBounds(limiteMap);
   mapGP.on("drag", function () {
     mapGP.panInsideBounds(limiteMap, { animate: false });
   });
+
+  const markerBahrain = L.icon({
+    iconUrl: "data/markerPneu.png",
+    iconSize: [30, 30],
+  });
+  L.marker([26.0325, 50.5106], { icon: markerBahrain })
+    .addTo(mapGP)
+    .on("click", function () {
+      mapGP.flyTo([26.0325, 50.5106], mapGP.getMaxZoom());
+    });
 }
 
 //-------------------------------Gestion du click sur le bouton de la carte des grands-prix--------------------------------------------------
@@ -40,4 +52,5 @@ boutonDecouvrirStatsGrandsPrix.addEventListener("click", function () {
   console.log("zebiiiiiii");
   document.querySelector("#stats").innerHTML = "";
   afficherMapGP();
+  creationMarker();
 });
