19f22b56c530b8a2b68cf1826dfce3554b00a97a
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-03-14 16:47:52 +0100

    ajout des fichier python et static

---
+++ py/test.py
@@ -0,0 +1,10 @@
+# import fastf1 
+# from fastf1 import plotting
+# from matplotlib import pyplot as plt
+# from matplotlib.pyplot import figure
+# from matplotlib.collections import LineCollection
+# from matplotlib import cm
+# import numpy as np
+# import pandas as pd
+
+print("hello world")
