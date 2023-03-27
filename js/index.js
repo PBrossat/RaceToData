e703a44c53e496f920a5a5090dc090d4f8d55014
Lucas <lucas.faustmann@gmail.com>
2023-03-27 09:23:08 +0200

    MAJ de la route permettant de récup les données télémtriques des pilotes pour la simulation dans index.js

--- js/index.js
+++ js/index.js
@@ -41,129 +41,158 @@ app.get("/infosGpJSON", (_, res) => {
   });
 });
 
-app.get("/infosGpDriver1", (req, res) => {
-  const positionXFile = "./json/Driver1/coordX.json";
-  const positionYFile = "./json/Driver1/coordY.json";
-  const timeFile = "./json/Driver1/time.json";
-  const speedFile = "./json/Driver1/speed.json";
-  const brakeFile = "./json/Driver1/brake.json";
-  const rpmFile = "./json/Driver1/rpm.json";
-  const gearFile = "./json/Driver1/gear.json";
-  const throttleFile = "./json/Driver1/throttle.json";
-  const drsFile = "./json/Driver1/drs.json";
-
-  const readJSONFile = (file) => {
-    return new Promise((resolve, reject) => {
-      fs.readFile(file, "utf8", (err, data) => {
-        if (err) reject(err);
-        resolve(JSON.parse(data));
-      });
-    });
-  };
-
-  Promise.all([
-    readJSONFile(positionXFile),
-    readJSONFile(positionYFile),
-    readJSONFile(timeFile),
-    readJSONFile(speedFile),
-    readJSONFile(brakeFile),
-    readJSONFile(rpmFile),
-    readJSONFile(gearFile),
-    readJSONFile(throttleFile),
-    readJSONFile(drsFile),
-  ])
-    .then((data) => {
-      const donnees = [];
-      data[0].forEach((posX, i) => {
-        const posY = data[1][i];
-        const time = data[2][i];
-        const speed = data[3][i];
-        const brake = data[4][i];
-        const rpm = data[5][i];
-        const gear = data[6][i];
-        const throttle = data[7][i];
-        const drs = data[8][i];
-        donnees.push({
-          positionX: posX,
-          positionY: posY,
-          time: time,
-          speed: speed,
-          brake: brake,
-          rpm: rpm,
-          gear: gear,
-          throttle: throttle / 100,
-          drs: drs,
-        });
-      });
-      res.json(donnees);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Internal server error");
-    });
+//fetch("http://localhost:3001/dataPython?namePilote=LEC&nameGP=Monaco&nameData=coordX")
+app.get("/dataPython", (req, res) => {
+  //récupération des données du formulaire
+  const namePilote = req.query.namePilote;
+  const nameGP = req.query.nameGP;
+  const nameData = req.query.nameData;
+  argv = [namePilote, nameGP, nameData];
+  let pyshell = new PythonShell("./app.py", { args: argv });
+  pyshell.on("message", function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    // console.log(message);
+    const data = message;
+    res.json(data);
+  });
+  // end the input stream and allow the process to exit
+  pyshell.end(function (err, code, signal) {
+    if (err) throw err;
+    console.log("The exit code was: " + code);
+    console.log("The exit signal was: " + signal);
+    console.log("finished");
+  });
 });
 
-app.get("/infosGpDriver2", (req, res) => {
-  const positionXFile = "./json/Driver2/coordX.json";
-  const positionYFile = "./json/Driver2/coordY.json";
-  const timeFile = "./json/Driver2/time.json";
-  const speedFile = "./json/Driver2/speed.json";
-  const brakeFile = "./json/Driver2/brake.json";
-  const rpmFile = "./json/Driver2/rpm.json";
-  const gearFile = "./json/Driver2/gear.json";
-  const throttleFile = "./json/Driver2/throttle.json";
-  const drsFile = "./json/Driver2/drs.json";
+//fetch("http://localhost:3001/dataDriver?namePilote=LEC&nameGP=Monaco")
+app.get("/dataDriver", async (req, res) => {
+  //récupération des données du formulaire
+  const namePilote = req.query.namePilote;
+  const nameGP = req.query.nameGP;
+  //récupération des données via les appels
+  try {
+    //coordX
+    let coordX = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=X"
+    );
+    coordX = await coordX.json();
+    //coordY
+    let coordY = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=Y"
+    );
+    coordY = await coordY.json();
+    //time
+    let time = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=Time"
+    );
+    time = await time.json();
+    //speed
+    let speed = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=Speed"
+    );
+    speed = await speed.json();
+    //brake
+    let brake = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=Brake"
+    );
+    brake = await brake.json();
+    //rpm
+    let rpm = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=RPM"
+    );
+    rpm = await rpm.json();
+    //gear
+    let gear = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=nGear"
+    );
+    gear = await gear.json();
+    //throttle
+    let throttle = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=Throttle"
+    );
+    throttle = await throttle.json();
+    //drs
+    let drs = await fetch(
+      "http://localhost:3001/dataPython?namePilote=" +
+        namePilote +
+        "&nameGP=" +
+        nameGP +
+        "&nameData=DRS"
+    );
+    drs = await drs.json();
 
-  const readJSONFile = (file) => {
-    return new Promise((resolve, reject) => {
-      fs.readFile(file, "utf8", (err, data) => {
-        if (err) reject(err);
-        resolve(JSON.parse(data));
+    //création du tableau de données
+    const donnees = [];
+    coordX = JSON.parse(coordX);
+    coordY = JSON.parse(coordY);
+    time = JSON.parse(time);
+    speed = JSON.parse(speed);
+    brake = JSON.parse(brake);
+    rpm = JSON.parse(rpm);
+    gear = JSON.parse(gear);
+    throttle = JSON.parse(throttle);
+    drs = JSON.parse(drs);
+
+    coordX.forEach((posX, i) => {
+      const posY = coordY[i];
+      const timeData = time[i];
+      const speedData = speed[i];
+      const brakeData = brake[i];
+      const rpmData = rpm[i];
+      const gearData = gear[i];
+      const throttleData = throttle[i] / 100;
+      const drsData = drs[i];
+      donnees.push({
+        positionX: posX,
+        positionY: posY,
+        time: timeData,
+        speed: speedData,
+        brake: brakeData,
+        rpm: rpmData,
+        gear: gearData,
+        throttle: throttleData,
+        drs: drsData,
       });
     });
-  };
-
-  Promise.all([
-    readJSONFile(positionXFile),
-    readJSONFile(positionYFile),
-    readJSONFile(timeFile),
-    readJSONFile(speedFile),
-    readJSONFile(brakeFile),
-    readJSONFile(rpmFile),
-    readJSONFile(gearFile),
-    readJSONFile(throttleFile),
-    readJSONFile(drsFile),
-  ])
-    .then((data) => {
-      const donnees = [];
-      data[0].forEach((posX, i) => {
-        const posY = data[1][i];
-        const time = data[2][i];
-        const speed = data[3][i];
-        const brake = data[4][i];
-        const rpm = data[5][i];
-        const gear = data[6][i];
-        const throttle = data[7][i];
-        const drs = data[8][i];
-        donnees.push({
-          positionX: posX,
-          positionY: posY,
-          time: time,
-          speed: speed,
-          brake: brake,
-          rpm: rpm,
-          gear: gear,
-          throttle: throttle / 100,
-          drs: drs,
-        });
-      });
-      res.json(donnees);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Internal server error");
-    });
+    res.json(donnees);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal server error");
+  }
 });
+
 // à reprendre
 app.get("/f1/:annee/:nomPiloteMinuscule", async (req, res) => {
   const annee = req.params.annee;
